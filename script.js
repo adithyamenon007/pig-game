fb1b95a013659c711290fcad98901250f289d4be
Adithya.S.Menon <62089022+adithyamenon007@users.noreply.github.com>
2021-12-02 20:11:37 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,5 +1,36 @@
 'use strict';
 //Initializing all variables
+
+//Game Rules
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnsOpenModal = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+for (let i = 0; i < btnsOpenModal.length; i++)
+  btnsOpenModal[i].addEventListener('click', openModal);
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  // console.log(e.key);
+
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
+    closeModal();
+  }
+});
+
 const section0 = document.querySelector('.player--0');
 const section1 = document.querySelector('.player--1');
 
